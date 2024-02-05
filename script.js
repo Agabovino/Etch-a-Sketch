d7378b815984c5283390182e6a7d6f343a883139
Agabovino <114683437+Agabovino@users.noreply.github.com>
2024-02-05 14:09:00 -0300

    added pixel destribution method

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
+let screen = document.querySelector('.screen');
+let resetBtn = document.querySelector('.reset-btn');
+
+let pixelArray = [];
+
+/*EVENT LISTENER DO BOTAO*/
+
+resetBtn.addEventListener('click', ()=>{
+
+    for(i = 0; i <= 256 ; i++){
+
+
+        if(i % 2 == 0){
+
+            pixelArray[i] = document.createElement('div');
+            pixelArray[i].classList.add('div-pixel-white');
+            screen.appendChild(pixelArray[i]);
+
+        }else{
+
+            pixelArray[i] = document.createElement('div');
+            pixelArray[i].classList.add('div-pixel-black');
+            screen.appendChild(pixelArray[i]);
+        }  
+    }   
+})
+
+
+
+
+
+
+
+
+
 
 
