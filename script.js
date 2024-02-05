8880adf0fe151f871cab75909a2d4c526ecafe22
Agabovino <114683437+Agabovino@users.noreply.github.com>
2024-02-05 16:59:53 -0300

    fixed bug #1

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ let screen = document.querySelector('.screen');
 let resetBtn = document.querySelector('.reset-btn');
 let startBtn = document.querySelector('.start-btn');
 let powerBtn = document.querySelector('.power-btn');
+let rightBtn = document.querySelector('.right-arrow-btn');
+let leftBtn = document.querySelector('.left-arrow-btn');
+
+let operatorNumber = document.querySelector('.pixel-display').textContent;
+operatorNumber = Number(operatorNumber);
+
+let stringWidthAndHeight = (((400/operatorNumber)/400)*100 ) + '%';
 
 let isItOn = false;
+
 const pixelArray = [];
 
 function setPixels(){
 
-    for(i = 0; i < 256 ; i++){
+    operatorNumber = document.querySelector('.pixel-display').textContent;
+    operatorNumber = Number(operatorNumber);
+    console.log(operatorNumber);
+
+    stringWidthAndHeight = (((400/operatorNumber)/400)*100 ) + '%';
+    console.log(stringWidthAndHeight);
+    
+    for(i = 0; i < operatorNumber*operatorNumber ; i++){
 
         pixelArray[i] = document.createElement('div');
         pixelArray[i].classList.add('div-pixel-white');
+        pixelArray[i].style.width = stringWidthAndHeight;
+        pixelArray[i].style.height = stringWidthAndHeight;
+
         screen.appendChild(pixelArray[i]);
     }  
 
@@ -20,10 +38,9 @@ function setPixels(){
 
 function deletePixels(){
     
-    for(i = 0; i < 256 ; i++){
-       
-        screen.removeChild(pixelArray[i]);
-    }  
+    pixelArray.forEach((pixel)=> {
+        pixel.remove();
+    });
 }
 
 /*EVENT LISTENER DOS BOTOES*/
@@ -60,3 +77,31 @@ powerBtn.addEventListener('click', ()=>{
     }
     
 })
+
+rightBtn.addEventListener('click', ()=>{
+
+    operatorNumber *= 2;
+    console.log(operatorNumber);
+    document.querySelector('.pixel-display').textContent = operatorNumber;
+    
+})
+
+leftBtn.addEventListener('click', ()=>{
+
+    operatorNumber /= 2;
+    console.log(operatorNumber);
+
+    document.querySelector('.pixel-display').textContent = operatorNumber;
+    
+})
+
+
+
+
+/* 160.000pixeis^2 
+
+ 16 = 6,25% de 400(valor total)
+ 32 = 3,125% de 400
+
+*/
+
