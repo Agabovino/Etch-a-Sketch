f65aab9219d5d19ae3c7d02bfa3a3d498dc7fa65
Agabovino <114683437+Agabovino@users.noreply.github.com>
2024-02-05 15:51:04 -0300

    oh crap i did everthing at one commit sorry

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 let screen = document.querySelector('.screen');
+
 let resetBtn = document.querySelector('.reset-btn');
+let startBtn = document.querySelector('.start-btn');
+let powerBtn = document.querySelector('.power-btn');
 
-let pixelArray = [];
+let isItOn = false;
+const pixelArray = [];
 
-/*EVENT LISTENER DO BOTAO*/
+function setPixels(){
+
+    for(i = 0; i < 256 ; i++){
+
+        pixelArray[i] = document.createElement('div');
+        pixelArray[i].classList.add('div-pixel-white');
+        screen.appendChild(pixelArray[i]);
+    }  
+
+}
+
+function deletePixels(){
+    
+    for(i = 0; i < 256 ; i++){
+       
+        screen.removeChild(pixelArray[i]);
+    }  
+}
+
+/*EVENT LISTENER DOS BOTOES*/
+
+startBtn.addEventListener('click', ()=>{
+    
+    pixelArray.forEach((pixel) => {
+        pixel.addEventListener('mouseover',()=>{
+            pixel.setAttribute('class', 'div-pixel-black');
+        });
+    });
+    
+})
 
 resetBtn.addEventListener('click', ()=>{
 
-    for(i = 0; i <= 256 ; i++){
+    for(let pixel of pixelArray){
 
-
-        if(i % 2 == 0){
-
-            pixelArray[i] = document.createElement('div');
-            pixelArray[i].classList.add('div-pixel-white');
-            screen.appendChild(pixelArray[i]);
-
-        }else{
-
-            pixelArray[i] = document.createElement('div');
-            pixelArray[i].classList.add('div-pixel-black');
-            screen.appendChild(pixelArray[i]);
-        }  
-    }   
+        pixel.setAttribute('class', 'div-pixel-white');
+    }
 })
 
+powerBtn.addEventListener('click', ()=>{
 
+    if(isItOn == false){
+        
+        setPixels();
+        isItOn = true;
 
+    }else{
 
-
-
-
-
-
-
-
+        deletePixels();
+        isItOn = false;
+    }
+    
+})
