9191456e3804773ff3407681aa54eb355c1b324e
Agabovino <114683437+Agabovino@users.noreply.github.com>
2024-02-06 13:50:59 -0300

    added colors + removed start btn

--- script.js
+++ script.js
@@ -44,34 +44,87 @@ function deletePixels(){
     isItOn = false;
 }
 
+function getColorAttribute(){
+
+   let number = Math.random();
+   number = Math.floor(number*10);
+   
+   
+    switch(number){
+
+        case 0: 
+        return '#000000';
+        
+        case 1: 
+        return '#FFFFFF';
+
+        case 2: 
+        return '#FF0000';
+
+        case 3: 
+        return '#00FF00';
+
+        case 4: 
+        return '#0000FF';
+
+        case 5: 
+        return '#FFFF00';
+
+        case 6: 
+        return '#00FFFF';
+
+        case 7: 
+        return '#FF00FF';
+
+        case 8: 
+        return '#C0C0C0';
+
+        case 9: 
+        return '#808080';
+    }
+}
+
 /*EVENT LISTENER DOS BOTOES*/
 
-startBtn.addEventListener('click', ()=>{
+/*startBtn.addEventListener('click', ()=>{
     
     pixelArray.forEach((pixel) => {
         pixel.addEventListener('mouseover',()=>{
            
-                pixel.setAttribute('class', 'div-pixel-black');
+                pixel.setAttribute('class', 'div-pixel-rgb');
+                pixel.style.backgroundColor  = getColorAttribute();
         });
     });
     
-})
+})*/
 
 resetBtn.addEventListener('click', ()=>{
-
-    for(let pixel of pixelArray){
-        pixel.setAttribute('class', 'div-pixel-white');
+    
+    for(let pixel of pixelArray){ 
+       
+        pixel.style.backgroundColor  = '#CAE5FF';
     }
 })
 
 powerBtn.addEventListener('click', ()=>{
     if(isItOn == false){   
         setPixels();
+
+        pixelArray.forEach((pixel) => {
+            pixel.addEventListener('mouseover',()=>{
+               
+                    pixel.setAttribute('class', 'div-pixel-rgb');
+                    pixel.style.backgroundColor  = getColorAttribute();
+            });
+        });
+
         isItOn = true;
     }else{
         deletePixels();
         isItOn = false;
     }
+
+
 })
 
 rightBtn.addEventListener('click', ()=>{
